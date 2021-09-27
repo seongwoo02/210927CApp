3d937a4cd1b9d0b52f34480080fd906a742bca3c
seongwoo <seongu658@naver.com>
2021-09-27 11:44:08 +0900

    64비트로 컴파일 완료한 Point 구조체 정의 및 const 포인터변수 예제 연습 완료

--- ex0927/Main.cpp
+++ ex0927/Main.cpp
@@ -1,16 +1,118 @@
 #include <stdio.h>
-void PrintInteger(int n)
+typedef struct _point
 {
-	for (int i = 0; i < n; ++i)
-		printf("Hello : %d ", i);
-	printf("\n");
+	int x;
+	int y;
+} Point;
+void InitPoint(Point* p, int x, int y)
+{
+	p->x = x;
+	p->y = y;
+}
+void UninitPoint(Point* p)
+{
+	p->x = 0;
+	p->y = 0;
+}
+void PrintPoint(const Point* p)
+{
+	printf("(%d,%d)\n", p->x, p->y);
+}
+void AddPoint(Point* p, int x, int y)
+{
+	p->x += x;
+	p->y += y;
 }
 int main()
 {
-	PrintInteger(100);
-	PrintInteger(100);
-	PrintInteger(100);
+	Point pt;
+	InitPoint(&pt, 2, 3);
+	PrintPoint(&pt);
+	AddPoint(&pt, 3, 3);
+	PrintPoint(&pt);
+	UninitPoint(&pt);
 }
+
+//#include <stdio.h>
+//void Increment(int* pn)
+//{
+//	++* pn;
+//}
+//void Print(const int* pn)
+//{
+//	printf("int : %d\n", *pn);
+//}
+//void Decrement(int* pn)
+//{
+//	--* pn;
+//}
+//void Reset(int* pn)
+//{
+//	*pn = 0;
+//}
+//int main()
+//{
+//	int n = 0;
+//
+//	Increment(&n);
+//	Increment(&n);
+//	Increment(&n);
+//	Print(&n);
+//
+//	Decrement(&n);
+//	Print(&n);
+//
+//	Reset(&n);
+//	Print(&n);
+//}
+
+//#include <stdio.h>
+//void PrintArray(const int* arr, int size)
+//{
+//	for (int i = 0;i < size; ++i)
+//		printf("[%d] : %d\n", i, arr[i]);
+//}
+//int main()
+//{
+//	int arr[3] = { 1,2,3 };
+//	int arr2[5] = {100,200,300,400,500};
+//
+//	PrintArray(arr, 3);
+//	PrintArray(arr2, 5);
+//	PrintArray(arr2 + 2, 2);
+//}
+
+//#include <stdio.h>
+//typedef struct _point
+//{
+//	int x;
+//	int y;
+//} Point;
+//void PrintPoint(const Point* arg)
+//{
+//	printf("(%d, %d)\n", arg->x, arg->y);
+//}
+//int main()
+//{
+//	Point pt1 = { 2,3 };
+//
+//	PrintPoint(&pt1);
+//}
+
+//#include <stdio.h>
+//void PrintString(const char* s)
+//{
+//	printf("String : %s\n", s);
+//}
+//int main()
+//{
+//	char buf1[] = "Hello!";
+//	const char* buf2 = "Hello!";
+//
+//	PrintString(buf1);
+//	PrintString(buf2);
+//	PrintString("Hello!");
+//}
 
 //#include <stdio.h>
 //void PrintInteger(int n)
