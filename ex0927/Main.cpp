a4bb54b8ebb0c66366b9252e34376dd4ab859ca8
seongwoo <seongu658@naver.com>
2021-09-27 10:38:44 +0900

    Main.cpp의 PrintInteger() 함수 완성하고 수정

--- ex0927/Main.cpp
+++ ex0927/Main.cpp
@@ -1,13 +1,32 @@
 #include <stdio.h>
-
+void PrintInteger(int n)
+{
+	for (int i = 0; i < n; ++i)
+		printf("Hello : %d\n", i);
+}
 int main()
 {
-	printf("Hello!\n");
-	printf("Hello!\n");
-	printf("Hello!\n");
-	printf("Hello!\n");
-	printf("Hello!\n");
+	PrintInteger(100);
 }
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	for (int i = 0; i < 10; ++i)
+//		printf("Hello : %d\n", i);
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("Hello!\n");
+//	printf("Hello!\n");
+//	printf("Hello!\n");
+//	printf("Hello!\n");
+//	printf("Hello!\n");
+//}
 
 //#include <stdio.h>
 //
